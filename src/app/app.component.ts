53d095a7319f37e9ad11d035875de1adbccfd5bc
Mariacabriles29 <mecp2992@gmail.com>
2025-02-11 14:34:57 -0400

    feat: Use data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 //import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'projectAngular';
   number: number = 0;
+  userName: string = 'Adrian';
 
   addOne() {
     this.number++;
