87033ee8c6269abc8346d49718769cdf8901aa3b
Mariacabriles29 <mecp2992@gmail.com>
2025-02-20 13:42:53 -0400

    feat: Routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieFormPageComponent } from './pages/movie-form-page/movie-form-page.component';
 
-export const routes: Routes = [];
+//aqui voy a agregar las rutas de mi aplicacion
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'movies',
+    component: MovieListComponent,
+  },
+  { path: 'create', component: MovieFormPageComponent },
+];
